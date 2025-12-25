c4f9a33dce21710ed42009dde61b116549fe624f
vadimarsenij638-droid <vadimarsenij638@gmail.com>
2025-12-25 23:38:33 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/assistant/dianaapp/src/main/java/com/assistant/diana/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.assistant.diana
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Тут пізніше буде наш інтерфейс з 3 лініями
+    }
+}
