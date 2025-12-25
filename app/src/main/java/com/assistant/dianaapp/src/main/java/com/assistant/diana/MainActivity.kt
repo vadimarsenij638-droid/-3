5bc790d1989617114051c76fa6ee38618c51fcfc
vadimarsenij638-droid <vadimarsenij638@gmail.com>
2025-12-26 00:16:23 +0200

    Update MainActivity.kt

--- app/src/main/java/com/assistant/dianaapp/src/main/java/com/assistant/diana/MainActivity.kt
+++ app/src/main/java/com/assistant/dianaapp/src/main/java/com/assistant/diana/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Тут пізніше буде наш інтерфейс з 3 лініями
+        // Поки що пустий екран для перевірки збірки
     }
 }
+
